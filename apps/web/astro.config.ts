01b11f2c617247bedc8125919f2190c26927b4ca
Jordan Sweeting <slurpyb@fastmail.com>
2025-03-04 22:24:45 +0800

    add stuffs

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,17 +1,7 @@
-import node from '@astrojs/node';
-import solidJs from '@astrojs/solid-js';
+
 import {defineConfig} from 'astro/config';
 
 export default defineConfig({
-    integrations: [solidJs({})],
-    output: 'static',
-    adapter: node({
-        mode: 'standalone',
-    }),
-    experimental: {
-        svg: true,
-    },
-    // vite: {
-    //     plugins: [tsconfigPaths({root: './'})],
-    // },
+    integrations: [],
+    output: 'static'
 });
